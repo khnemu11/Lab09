f06856e697bee74f5ac74692542d513e4e43821d
Kim SuHyun <khnemu@naver.com>
2018-04-21 12:12:42 +0900

    change array to arraylist. and change access level in salesassociate

--- src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -7,35 +7,35 @@ package edu.handong.csee.java.lab09; //package edu.handong.csee.java.lab09
  *
  */
 public class SalesAssociate {
-	String name=null; //declare and initialize name;
-	double sales=0; //declare and initialize sales
+	private String name=null; //declare and initialize name;
+	private double sales=0; //declare and initialize sales
 
 	/**
 	 * set name value
 	 * @param name
 	 */
-	void setName(String name) {
+	public void setName(String name) {
 		this.name=name; //save input value in this.name
 	}
 	/**
 	 * set sales value
 	 * @param sales
 	 */
-	void setSales(double sales) {
+	public void setSales(double sales) {
 		this.sales=sales; ///save input value in this.sales
 	}
 	/**
 	 * return this.name
 	 * @return this.name
 	 */
-	String getName() {
+	public String getName() {
 		return this.name; //return this.name
 	}
 	/**
 	 * return this.sales
 	 * @return this.sales
 	 */
-	double getSales() {
+	public double getSales() {
 		return this.sales; //return this.sales
 	}
 }
--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -1,5 +1,5 @@
 package edu.handong.csee.java.lab09; // package edu.handong.csee.java.lab09
- 
+import java.util.ArrayList; //import that for using arraylist
 import java.util.Scanner; // import java.util.Scanner to use scanner
 
 /**
@@ -10,29 +10,40 @@ import java.util.Scanner; // import java.util.Scanner to use scanner
 public class SalesReporter {
 	private double highestSales; //declare private, double highestSales
 	private double averageSales; //declare private, double averageSales
-	private SalesAssociate[] team; //declare private array, team in SalesAssociate
-	private int numberOfAssociates; //declare private the maximum size of team
+	private ArrayList<SalesAssociate> team; //declare private array, team in SalesAssociate
 	/**
 	 * get data,name and sales, and save in array team of SalesAssociate
 	 */
 	public void getData(){
 		Scanner myScanner = new Scanner(System.in); //make object of Scanner
+		team= new ArrayList<SalesAssociate>(); //make arraylist
 		
-		System.out.print("Enter number of sales associate: "); //ask the maximum size of team;
-		numberOfAssociates=myScanner.nextInt(); //save in numberOfAssociates to Scanner
+		System.out.println("Enter items for the list, when prompted");
+		//ask the maximum size of team;
 		
-		team= new SalesAssociate[numberOfAssociates] ; //make array which size is numberOfAssociates
-		
-		for(int i=0; i<numberOfAssociates;i++) { //repeat under until i is bigger than numberOfAssociates
-			team[i]=new SalesAssociate(); //make object, team[i], in SalesAssociate
-
-			System.out.println("Enter data for associate number "+(i+1)); //show the count of associate number
+		while(true) {
+			SalesAssociate information = new SalesAssociate(); //instantiation information following by SalesAssociate
+			
 			System.out.print("Enter name of sales associate: "); //ask name
-			team[i].setName(myScanner.nextLine()); //buffer of Scanner. because nextLine is response upper enter.
-			team[i].setName(myScanner.nextLine()); //save name in team[i].name
+			information.setName(myScanner.nextLine()); //save name in information.name
 			
 			System.out.print("Enter associate's sales: $"); //ask sales
-			team[i].setSales(myScanner.nextDouble()); //save sales in team[i].Sales
+			information.setSales(myScanner.nextDouble()); //save sales in information.sales
+			String stop=myScanner.nextLine(); //buffter of stop that is variable for stop or continue this loop.
+			
+			team.add(information); //add information to team
+			
+			while(true) {
+				System.out.print("More items for the list? "); //ask more inputing item
+				stop=myScanner.nextLine(); //variable for stop or continue
+				
+				if("yes".equals(stop)==true)	break; //if it is yes, break and repeat upper loop
+				else if("no".equals(stop)==true)	return;// if it is no, finish method, getdata()
+				else {
+					System.out.println("you can input only yes or no"); //if stop is neither yes nor no, print this.
+					continue; //ask one more
+				}
+			}
 		}
 	}
 	/**
@@ -41,14 +52,13 @@ public class SalesReporter {
 	public void computeStats(){
 		int total=0; //declare and initialize total
 		 
-		for(int i=0;i<numberOfAssociates;i++) total+=team[i].sales; //repeat to sum all sales
+		for(SalesAssociate sales:team) total+=sales.getSales(); //calculate total sales until end of team
 		
-		averageSales=(double)total/numberOfAssociates; //divide total to numberOfAssociates
+		averageSales=(double)total/team.size(); //calculate averagesales
+		highestSales=0; //initialize the highest sales value
 		
-		highestSales=team[0].sales; //initialize the highest sales value
-		
-		for(int i=1;i<numberOfAssociates;i++) { 
-			if(highestSales<team[i].sales)	highestSales=team[i].sales; //find the highest sales 
+		for(SalesAssociate sales:team) { //loop for find highest sales in team
+			if(highestSales<sales.getSales())	highestSales=sales.getSales(); //save the highest sales 
 		}
 	}
 	/**
@@ -57,24 +67,24 @@ public class SalesReporter {
 	public void displayResults() {
 		System.out.println("Average sales per associate is $"+averageSales); //show average
 		System.out.println("The highest sales figure is $"+highestSales); //show the highest sales
-		for(int i=0; i<numberOfAssociates;i++) { //repeat to show the highest salesman
-			if(team[i].sales==highestSales) { //find until the highest sales
+		for(SalesAssociate information:team) { //repeat to show the highest salesman
+			if(information.getSales()==highestSales) { //find until the highest sales
 				System.out.println("The following had the highest sales:"); //show that sentence
-				System.out.println("name: "+team[i].name); //show the highest salesman's name
-				System.out.println("Sales: $"+team[i].sales); //show the highest salesman's sales 
-				System.out.println("$"+Math.round(team[i].sales-averageSales)+" above the average."); //show sales compare with average
+				System.out.println("name: "+information.getName()); //show the highest salesman's name
+				System.out.println("Sales: $"+information.getSales()); //show the highest salesman's sales 
+				System.out.println("$"+Math.round(information.getSales()-averageSales)+" above the average."); //show sales compare with average
 				break; //break
 			}
 		}
 		System.out.println("The rest performed as follows"); //show rest value
-		for(int i=0;i<numberOfAssociates;i++) { //repeat until show all rests
-			if(team[i].sales==highestSales) continue; //if the value is the highest, ignore
+		for(SalesAssociate information:team) { //repeat until show all rests
+			if(information.getSales()==highestSales) continue; //if the value is the highest, ignore
 			else { //if not
-			System.out.println("name: "+team[i].name); //show name
-			System.out.println("Sales: $"+team[i].sales);//show sales
+			System.out.println("name: "+information.getName()); //show name
+			System.out.println("Sales: $"+information.getSales());//show sales
 			
-			if(team[i].sales>averageSales)	System.out.println("$"+Math.round(team[i].sales-averageSales)+" above the average."); //if his sales is upper than average, show average to sales-average
-			else System.out.println("$"+Math.round(averageSales-team[i].sales)+" below the average."); ////if his sales is under than average, show average to average-sales
+			if(information.getSales()>averageSales)	System.out.println("$"+Math.round(information.getSales()-averageSales)+" above the average."); //if his sales is upper than average, show average to sales-average
+			else System.out.println("$"+Math.round(averageSales-information.getSales())+" below the average."); ////if his sales is under than average, show average to average-sales
 			}
 		}		
 	}
